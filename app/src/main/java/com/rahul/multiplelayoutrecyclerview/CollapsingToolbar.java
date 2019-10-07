3de060277f599aff9beb337faafbcc11d48a6dc0
RahulSinghRawatDev <rahul.rawat.er@gmail.com>
2019-10-07 15:25:25 +0530

    add multiple view inside recyclerview

---
+++ app/src/main/java/com/rahul/multiplelayoutrecyclerview/CollapsingToolbar.java
@@ -0,0 +1,14 @@
+package com.rahul.multiplelayoutrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollapsingToolbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collapsing_toolbar);
+    }
+}
---
+++ app/src/main/java/com/rahul/multiplelayoutrecyclerview/CoordinateActivite.java
@@ -0,0 +1,14 @@
+package com.rahul.multiplelayoutrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoordinateActivite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinate_activite);
+    }
+}
